89779f8db06bf18100f179dcde8dea8371cd93fd
Viktors Telle <viktors.telle@gmail.com>
2017-11-06 23:16:09 +0200

    Added migration for ReleaseNotes table.

--- ReleaseNotesGenerator/Migrations/20171106211431_AddReleaseNotesTable.Designer.cs
+++ ReleaseNotesGenerator/Migrations/20171106211431_AddReleaseNotesTable.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ReleaseNotesGenerator.Migrations
 {
     [DbContext(typeof(ReleaseNotesContext))]
-    partial class ReleaseNotesContextModelSnapshot : ModelSnapshot
+    [Migration("20171106211431_AddReleaseNotesTable")]
+    partial class AddReleaseNotesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ReleaseNotesGenerator/Migrations/20171106211431_AddReleaseNotesTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ReleaseNotesGenerator.Migrations
+{
+    public partial class AddReleaseNotesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReleaseNotes",
+                schema: "rng",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RepositoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReleaseNotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReleaseNotes_Repositories_RepositoryId",
+                        column: x => x.RepositoryId,
+                        principalSchema: "rng",
+                        principalTable: "Repositories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReleaseNotes_RepositoryId",
+                schema: "rng",
+                table: "ReleaseNotes",
+                column: "RepositoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReleaseNotes",
+                schema: "rng");
+        }
+    }
+}
