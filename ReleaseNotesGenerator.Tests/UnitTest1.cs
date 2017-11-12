b0563695e2ea8611dfdafdf7ae5f87d3a58e7e63
Viktors Telle <viktors.telle@gmail.com>
2017-11-12 21:55:02 +0200

    Added unit test project.

---
+++ ReleaseNotesGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ReleaseNotesGenerator.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]        
+        public void TestMethod1()
+        {
+        }
+    }
+}
