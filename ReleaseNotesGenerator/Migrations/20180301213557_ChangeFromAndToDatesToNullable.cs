f76bc6e4fccfe391f9003da9c4cf509a4cc3e259
Viktors Telle <viktors.telle@gmail.com>
2018-03-01 23:37:34 +0200

    Updated nuget packages and made some improvements.

---
+++ ReleaseNotesGenerator/Migrations/20180301213557_ChangeFromAndToDatesToNullable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ReleaseNotesGenerator.Migrations
+{
+    public partial class ChangeFromAndToDatesToNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Until",
+                schema: "rng",
+                table: "ReleaseNotes",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "From",
+                schema: "rng",
+                table: "ReleaseNotes",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Until",
+                schema: "rng",
+                table: "ReleaseNotes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "From",
+                schema: "rng",
+                table: "ReleaseNotes",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
